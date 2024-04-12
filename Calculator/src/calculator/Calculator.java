1f6edd4b66855826401437000872125824d3063a
GopherLoaf <165398272+GopherLoaf@users.noreply.github.com>
2024-04-12 16:41:37 -0400

    Add files via upload

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author weig4542
+ */
+public class Calculator extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Variables
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        Image icon = new Image("Calculator.png");
+        
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        
+        //Initialize
+        stage.setTitle("Calculator");
+        stage.getIcons().add(icon);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
